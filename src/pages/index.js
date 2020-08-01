3306b0c9f83cbe53cd39dfd888125f7756a94441
Homan13 <homan13@gmail.com>
2020-08-01 14:50:30 -0400

    Update to PayPal button
    - Converted to pure HTML for the PayPal button instead of a seperate
    component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,9 +42,35 @@ const IndexPage = () => (
         <div className="row">
           <div className="col-lg-8 mx-auto">
             <h2 className="text-white mb-4">Purchase your Esotericon 2021 tickets now!</h2>
-            <p className="text-white-50">
-                <PaypalExpressBtn />
-            </p>
+            <div className="mx-auto text-center">
+              <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
+                <input type="hidden" name="cmd" value="_cart" />
+                <input type="hidden" name="business" value="jmartinezafam@gmail.com" />
+                <input type="hidden" name="lc" value="US" />
+                <input type="hidden" name="item_name" value="Ticket Type" />
+                <input type="hidden" name="button_subtype" value="products" />
+                <input type="hidden" name="no_note" value="0" />
+                <input type="hidden" name="currency_code" value="USD" />
+                <input type="hidden" name="add" value="1" />
+                <input type="hidden" name="bn" value="PP-ShopCartBF:btn_cart_LG.gif:NonHostedGuest" />
+                  <table>
+                    <tr><td><input type="hidden" name="on0" value="Ticket Type" />Ticket Type</td></tr><tr><td><select name="os0">
+                    <option value="Virtual Only (Donation)">Virtual Only (Donation) $1.00 USD</option>
+                    <option value="In Person (Donation)">In Person (Donation) $1.25 USD</option>
+                    <option value="Executive (Donation)">Executive (Donation) $1.50 USD</option>
+                    </select> </td></tr>
+                  </table>
+                <input type="hidden" name="option_select0" value="Virtual Only (Donation)" />
+                <input type="hidden" name="option_amount0" value="1.00" />
+                <input type="hidden" name="option_select1" value="In Person (Donation)" />
+                <input type="hidden" name="option_amount1" value="1.25" />
+                <input type="hidden" name="option_select2" value="Executive (Donation)" />
+                <input type="hidden" name="option_amount2" value="1.50" />
+                <input type="hidden" name="option_index" value="0" />
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+              </form>
+            </div>
           </div>
         </div>
       </div>
