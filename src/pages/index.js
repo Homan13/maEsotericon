d033b3beb2439cb0e0ed119712a9603d38d19808
Homan13 <homan13@gmail.com>
2020-08-01 15:10:36 -0400

    Minor table updates to PayPal button

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,30 +43,20 @@ const IndexPage = () => (
           <div className="col-lg-8 mx-auto">
             <h2 className="text-white mb-4">Purchase your Esotericon 2021 tickets now!</h2>
             <div className="mx-auto text-center">
-              <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
-                <input type="hidden" name="cmd" value="_cart" />
-                <input type="hidden" name="business" value="jmartinezafam@gmail.com" />
-                <input type="hidden" name="lc" value="US" />
-                <input type="hidden" name="item_name" value="Ticket Type" />
-                <input type="hidden" name="button_subtype" value="products" />
-                <input type="hidden" name="no_note" value="0" />
-                <input type="hidden" name="currency_code" value="USD" />
-                <input type="hidden" name="add" value="1" />
-                <input type="hidden" name="bn" value="PP-ShopCartBF:btn_cart_LG.gif:NonHostedGuest" />
+              <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input type="hidden" name="hosted_button_id" value="5B7KJXWPUPEMY" />
                   <table>
-                    <tr><td><input type="hidden" name="on0" value="Ticket Type" />Ticket Type</td></tr><tr><td><select name="os0">
-                    <option value="Virtual Only (Donation)">Virtual Only (Donation) $1.00 USD</option>
-                    <option value="In Person (Donation)">In Person (Donation) $1.25 USD</option>
-                    <option value="Executive (Donation)">Executive (Donation) $1.50 USD</option>
+                    <tr><td><input type="hidden" name="on0" value="2021 MAE Ticket(s)" />2021 MAE Ticket(s)</td></tr><tr><td><select name="os0">
+	                    <option value="Virtual Only (Donation)">Virtual Only (Donation) $1.00 USD</option>
+	                    <option value="Virtual + Swag + Shipping (Donation)">Virtual + Swag + Shipping (Donation) $2.00 USD</option>
+	                    <option value="In-Person + Swag (Donation)">In-Person + Swag (Donation) $3.00 USD</option>
+	                    <option value="In-Person + Swag + Brew Night (Donation)">In-Person + Swag + Brew Night (Donation) $5.00 USD</option>
+	                    <option value="VIP Pass (Donation)">VIP Pass (Donation) $450.00 USD</option>
+	                    <option value="Tarot Reading (Donation)">Tarot Reading (Donation) $5.00 USD</option>
                     </select> </td></tr>
                   </table>
-                <input type="hidden" name="option_select0" value="Virtual Only (Donation)" />
-                <input type="hidden" name="option_amount0" value="1.00" />
-                <input type="hidden" name="option_select1" value="In Person (Donation)" />
-                <input type="hidden" name="option_amount1" value="1.25" />
-                <input type="hidden" name="option_select2" value="Executive (Donation)" />
-                <input type="hidden" name="option_amount2" value="1.50" />
-                <input type="hidden" name="option_index" value="0" />
+                <input type="hidden" name="currency_code" value="USD" />
                 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
                 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
               </form>
