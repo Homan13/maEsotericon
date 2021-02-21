4e0ef9e2684561f3afe8c08dfcf8ff727cbad067
Homan13 <homan13@gmail.com>
2021-02-21 13:22:07 -0500

    Site updated to version 1.8.0 - PayPal link added for 2021 ticket sales!

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,6 +35,24 @@ const IndexPage = () => (
       </div>
     </header>
 
+    <section id="purchase" className="about-section text-center">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 mx-auto">
+            <h2 className="text-white mb-4">Purchase your Esotericon 2021 tickets now!</h2>
+            <div className="mx-auto text-center">
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input type="hidden" name="hosted_button_id" value="E8X9QSWCV8SKN" />
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <section id="about" className="about-section text-center">
       <div className="container">
         <div className="row">
