96802dff7cd1810d6e58f02740eeed80d8f664d1
Homan13 <homan13@gmail.com>
2021-02-22 15:59:32 -0500

    Last test run

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,17 +69,9 @@ const IndexPage = () => (
             <div className="mx-auto text-center">
               <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
                 <input type="hidden" name="cmd" value="_s-xclick" />
-                <input type="hidden" name="hosted_button_id" value="G3ZD3GP8ABR2N" />
-                  <table>
-                    <tr><td><input type="hidden" name="on0" value="Number of Tickets" />Number of Tickets</td></tr><tr><td><select name="os0">
-                    <option value="TEST 1">TEST 1 $1.00 USD</option>
-                    <option value="TEST 2">TEST 2 $2.00 USD</option>
-                    <option value="TEST 3">TEST 3 $3.00 USD</option>
-                    </select> </td></tr>
-                  </table>
-                    <input type="hidden" name="currency_code" value="USD" />
-                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
-                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                <input type="hidden" name="hosted_button_id" value="YP2FV5FD45ANC" />
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
               </form>
             </div>
           </div>
